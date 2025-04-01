4917ab1515aea7524312f8008b43d80930a4bebd
pmalimba <pmalimba@yahoo.com>
2025-04-01 22:01:37 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {""}
+          <a href="#" target="_blank">
+            Paula Malimba {""}
+          </a>
+          and is{" "}
+          <a href="#" target="_blank">
+            open-sourced on GitHub {""}
+          </a>
+          and{" "}
+          <a href="" target="_blank">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
