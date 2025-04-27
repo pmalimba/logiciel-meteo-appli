bf60b8559ed751d58fd17c3b3a150106d73217a0
pmalimba <pmalimba@yahoo.com>
2025-04-27 20:14:49 +0200

    Corrected a line

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -27,7 +27,7 @@ export default function WeatherForecast(props) {
         <div className="row">
           <div className="col">
             <div className="WeatherForecast-day">Thu</div>
-            <WeatherIcon code="01d" size={36} />
+            <WeatherIcon code="Clear Sky" size={36} />
             <div className="WeatherForecast-temperatures">
               <span className="WeatherForecast-temperatures-max">
                 {Math.round(forecast[0].temp.max)}°
