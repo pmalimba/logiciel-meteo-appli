eeb5a7e4c09203b1de16d58952e072e4ffa63bda
pmalimba <pmalimba@yahoo.com>
2025-04-29 19:43:29 +0200

    Corrections

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
-import WeatherIcon from "./WeatherIcon";
 import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
